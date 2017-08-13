0b900ce3cbf2e65c43ddb5360828e5382b3f08d4
Anshul Sharma <30723951+Ansvas@users.noreply.github.com>
2017-08-13 19:14:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/myprofile',function(req,res)
+{
+    res.send('And here is my profile');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
