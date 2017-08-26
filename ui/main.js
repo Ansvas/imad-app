53c602c8a5042c4f67ea4e53d0614e71df89a048
Anshul Sharma <30723951+Ansvas@users.noreply.github.com>
2017-08-27 00:23:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+var nam=document.getElementById("name").value;
+var username = document.getElementById("un").value;
+var password= document.getElementById("pass").value;
+document.write("ur name is"+ nam+"<br/>");
+document.write("ur userid is"+ username+"<br/>");
