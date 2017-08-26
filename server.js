d59dcb3c232fec45a8bb4cae1c6eddb9e1bf1940
Anshul Sharma <30723951+Ansvas@users.noreply.github.com>
2017-08-26 13:52:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article2', function(req,res){
     
 });
 app.get('/article3', function(req,res){
-   res.send('article 3 is requested and succesfuly served'); 
+   res.sendFile(path.join(__dirname,'ui','article3.html')); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
